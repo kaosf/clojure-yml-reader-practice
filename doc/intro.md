da24e66b6d3ce9c055dcf9baa05acdcaebda9d54
ka <ka.kaosf@gmail.com>
2014-02-14 02:33:19 +0900

    initialize
    execute "lein new app yml-reader-practice"
    
    Leiningen version is 2.3.4

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to yml-reader-practice
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/yml_reader_practice/core_test.clj
@@ -0,0 +1,7 @@
+(ns yml-reader-practice.core-test
+  (:require [clojure.test :refer :all]
+            [yml-reader-practice.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
